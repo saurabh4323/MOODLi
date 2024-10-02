c17fa5aa34499268400ea3448d8c79cc077d9c19
saurabh4323 <saurabhiitr01@gmail.com>
2024-10-02 13:09:00 +0530

    fff

--- app/layout.js
+++ app/layout.js
@@ -56,6 +56,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-8925928685447144"
+        ></meta>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>MOODLI</title>
