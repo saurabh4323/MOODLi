6645e84001f528d700fa1e1aa27c2fdf853e8913
saurabh4323 <saurabhiitr01@gmail.com>
2024-10-06 13:25:31 +0530

    r

--- components/Heros.jsx
+++ components/Heros.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import styles from "./Heros.module.css"; // Import the CSS file
 import Hero from "./Hero";
@@ -25,13 +26,21 @@ const Heros = () => {
 
         <div className={styles.content}>
           <h1 className={styles.title}>
-            Elevate Your Emotional Well-being <br /> with our Mood Tracker App
+            Elevate Your Emotion
+            <br /> with our Moodli
           </h1>
           <p className={styles.subtitle}>
             Empower Yourself with Self-awareness, Track Your Emotions, <br />
-            and Unleash a Happier You
+            chat with community and Unleash a Happier You
           </p>
-          <button className={styles.button}>Join with us</button>
+          <button
+            className={styles.button}
+            onClick={() => {
+              window.location.href = "/login";
+            }}
+          >
+            Join with us
+          </button>
           <Hero></Hero>
         </div>
         <div className={styles.emotionsSection}>
@@ -88,12 +97,6 @@ const Heros = () => {
             />
           </div>
         </div>
-        {/* <div
-          className="fea"
-          style={{ height: "20vh", backgroundColor: "#000" }}
-        >
-          h
-        </div> */}
       </div>
     </div>
   );
