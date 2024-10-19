5ab5cdd2d7d71f4ca23ed07a8d020e0ce5a020d1
saurabh4323 <saurabhiitr01@gmail.com>
2024-10-20 01:06:02 +0530

    rer

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Heros from "@/components/Heros";
 import Image from "next/image";
 import Head from "next/head";
+import Newhero from "@/components/Afterchat/Newhero";
 
 export default function Home() {
   return (
     <div>
-      <Heros></Heros>
+      {/* <Heros></Heros>
+       */}
+      <Newhero></Newhero>
     </div>
   );
 }
