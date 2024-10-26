b88132a6f9a4bcb5efa3f1772534bd40f9e1e4f8
saurabh4323 <saurabhiitr01@gmail.com>
2024-10-27 00:14:16 +0530

    R

--- app/create/page.js
+++ app/create/page.js
@@ -5,39 +5,41 @@ import Chat from "@/components/Chat";
 import CustomEmoji from "@/components/CustomEmoji";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-export default function page() {
+export default function Page() {
   const rouuter = useRouter();
   useEffect(() => {
-    const checkAuthentication = async () => {
-      const userId = localStorage.getItem("userId");
-      const storedEmail = localStorage.getItem("email");
+    if (typeof window !== "undefined") {
+      const checkAuthentication = async () => {
+        const userId = localStorage.getItem("userId");
+        const storedEmail = localStorage.getItem("email");
 
-      if (!userId || !storedEmail) {
-        alert("Please log in to access your profile.");
-        rouuter.push("/login");
-        return;
-      }
-
-      try {
-        const response = await axios.post("/api/users/sau", { userId });
-        if (response.data.email !== storedEmail) {
-          alert("Don't try this again");
-          localStorage.removeItem("userId");
-          localStorage.removeItem("email");
+        if (!userId || !storedEmail) {
+          alert("Please log in to access your profile.");
           rouuter.push("/login");
-        } else {
-          setProfile(response.data);
+          return;
         }
-      } catch (error) {
-        console.error("Error verifying user email:", error);
-        // alert("Authentication error. Redirecting to login.");
-        // rouuter.push("/login");
-      } finally {
-        setLoading(false);
-      }
-    };
 
-    checkAuthentication();
+        try {
+          const response = await axios.post("/api/users/sau", { userId });
+          if (response.data.email !== storedEmail) {
+            alert("Don't try this again");
+            localStorage.removeItem("userId");
+            localStorage.removeItem("email");
+            rouuter.push("/login");
+          } else {
+            setProfile(response.data);
+          }
+        } catch (error) {
+          console.error("Error verifying user email:", error);
+          // alert("Authentication error. Redirecting to login.");
+          // rouuter.push("/login");
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      checkAuthentication();
+    }
   }, []);
   return (
     <div>
